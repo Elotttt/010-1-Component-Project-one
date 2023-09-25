4c8b1079c629bddce4f13f58f7b723eba1df25ad
Elotttt <elliot@jrfry.co.uk>
2023-09-25 15:49:16 +0100

    Initial commit
    Testing in a blank scene

---
+++ 010-1 Component Project one/Assets/My Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 0.25f;
+    float snapDistance = 0.25f;
+    float rayLength = 1.4f;
+    float rayOffsetX = 0.5f;
+    float rayOffsetY = 0.5f;
+    float rayOffsetZ = 0.5f;
+
+    Vector3 targetPosition;
+    Vector3 startPosition;
+
+    bool moving;
+
+    private void Update()
+    {
+        if (moving)
+        {
+            if (Vector3.Distance(startPosition, transform.position) > 1f)
+            {
+                transform.position = targetPosition;
+                moving = false;
+                return;
+            }
+
+            transform.position += (targetPosition - startPosition) * moveSpeed * Time.deltaTime;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (CanMove(Vector3.forward))
+            {
+                targetPosition = transform.position + Vector3.forward;
+                startPosition = transform.position;
+                moving = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (CanMove(Vector3.left))
+            {
+                targetPosition = transform.position + Vector3.left;
+                startPosition = transform.position;
+                moving = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (CanMove(Vector3.back))
+            {
+                targetPosition = transform.position + Vector3.back;
+                startPosition = transform.position;
+                moving = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (CanMove(Vector3.right))
+            {
+                targetPosition = transform.position + Vector3.right;
+                startPosition = transform.position;
+                moving = true;
+
+            }
+        }
+    }
+
+    private bool CanMove(Vector3 direction)
+    {
+        if (Vector3.Equals(Vector3.forward, direction) || Vector3.Equals(Vector3.back, direction))
+        {
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.right * rayOffsetX, direction, rayLength))
+            {
+                return false;
+            }
+            if(Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.right * rayOffsetX, direction, rayLength))
+            {
+                return false;
+            }
+        }
+        else if (Vector3.Equals(Vector3.left, direction) || Vector3.Equals(Vector3.right, direction))
+        {
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY + Vector3.forward * rayOffsetZ, direction, rayLength))
+            {
+                return false;
+            }
+            if (Physics.Raycast(transform.position + Vector3.up * rayOffsetY - Vector3.forward * rayOffsetZ, direction, rayLength))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
